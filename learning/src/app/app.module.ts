37aab2504e76fab506d193d899214585f625e64a
SimonKovacsp22 <postcardsent@gmail.com>
2023-04-27 22:51:12 +0200

    just styling navbar and homepage

--- learning/src/app/app.module.ts
+++ learning/src/app/app.module.ts
@@ -1,22 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
 import { HomeComponent } from './home/home.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    NavComponent,
-    HomeComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, NavComponent, HomeComponent],
+  imports: [BrowserModule, AppRoutingModule, FontAwesomeModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- learning/src/app/nav/nav.component.ts
+++ learning/src/app/nav/nav.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
+import { faCartShopping, faOtter } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
-  styleUrls: ['./nav.component.css']
+  styleUrls: ['./nav.component.css'],
 })
 export class NavComponent {
+  faCartShopping = faCartShopping;
+  faOtter = faOtter;
+  cartModalVisible = true;
+  onCartHover = () => (this.cartModalVisible = false);
 
+  onCartLeave = () => (this.cartModalVisible = true);
 }
