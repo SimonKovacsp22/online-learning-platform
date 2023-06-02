20f6be40901009f62f78e2f317c40a5da2ea1a69
SimonKovacsp22 <postcardsent@gmail.com>
2023-06-02 13:10:53 +0200

    added ratings
    dynamic sections and lectures in learn course component

--- learning/src/app/tab-overview/tab-overview.component.ts
+++ learning/src/app/tab-overview/tab-overview.component.ts
@@ -1,19 +1,27 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, NgModule } from '@angular/core';
 import { NgbNavModule } from '@ng-bootstrap/ng-bootstrap';
-import { IRating } from '../models/rating.model';
 import { ICourse } from '../models/course.model';
-import { NgIf } from '@angular/common';
+import { NgFor, NgIf, DatePipe } from '@angular/common';
 import { CourseService } from '../services/course/course.service';
-import { LoginService } from '../services/login/login.service';
+import { faChevronDown, faStar } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+
 @Component({
   selector: 'app-tab-overview',
   templateUrl: './tab-overview.component.html',
   standalone: true,
-  imports: [NgbNavModule, NgIf],
+  imports: [NgbNavModule, NgIf, FontAwesomeModule, NgFor],
   styleUrls: ['./tab-overview.component.css'],
 })
 export class TabOverviewComponent {
-  activeTab = 1;
+  activeTab = 3;
   @Input() course: ICourse | null = null;
+  faChevronDown = faChevronDown;
+  faStar = faStar;
   constructor(public courseService: CourseService) {}
+
+  formatDate(dateString: string) {
+    const date = new Date(dateString);
+    return Intl.DateTimeFormat('en-US').format(date);
+  }
 }
